4cdbc2533c271be299ed28455f5cd535e2551479
SinesioBittencourt <contato@sinesio.com.br>
2016-03-20 18:38:16 -0300

    up

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,8 @@ router.post('/invite', function(req, res) {
             });
             return;
           } else if (error === 'invalid_email') {
-            error = 'The email you entered is an invalid email.'
+            //error = 'The email you entered is an invalid email.'
+            error = #{ERROR_ENVI}
           } else if (error === 'invalid_auth') {
             error = 'Something has gone wrong. Please contact a system administrator.'
           }
