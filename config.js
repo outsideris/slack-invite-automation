d2cf6046e91895741357d5f3b02274e70103c1b7
Bernardo <bernardo.csantos@gmail.com>
2016-04-22 21:06:33 -0300

    Primeiros ajustes no projetos

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  community: process.env.COMMUNITY_NAME || 'Floripa Gophers',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  slackUrl: process.env.SLACK_URL || 'floripa-gophers.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
@@ -16,5 +16,5 @@ module.exports = {
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
 
-  locale: process.env.LOCALE || "en",
+  locale: process.env.LOCALE || "pt",
 };
