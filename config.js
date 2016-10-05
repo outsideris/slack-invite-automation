a497e2bbb1ff899862b8bd926bd2ccc1d0916bc6
Outsider <outsideris@gmail.com>
2016-10-06 03:27:21 +0900

    rollback config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // your community or team name to display on join page.
+  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  // your slack team url (ex: socketio.slack.com)
+  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  // access token of slack
+  // You can generate it in https://api.slack.com/web#auth
+  // You should generate the token in admin user, not owner.
+  // If you generate the token in owner user, missing_scope error will be occurred.
+  //
+  // You can test your token via curl:
+  //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
+  //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
+  //   --compressed
+  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
+  // an optional security measure - if it is set, then that token will be required to get invited.
+  inviteToken: process.env.INVITE_TOKEN || null,
+
+  locale: process.env.LOCALE || "en",
+};
