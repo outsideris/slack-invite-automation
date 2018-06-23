c9cc4ba621eafb07e3c2676b5108e03868ae9381
Joe Lafiosca <joe@lafiosca.com>
2018-06-23 13:11:09 -0400

    add AWS deployment

---
+++ aws/config.example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+StackName=SlackInviteAutomation
+
+S3BucketArtifacts=your-artifact-bucket
+S3PrefixArtifacts=cloudformation/slack-invite-automation
+
+CommunityName="Your Community Name"
+SlackUrl=yourcommunity.slack.com
+SlackToken=xoxp-xxx-xxx-xxx-xxx
+InviteToken=
+RecaptchaSiteKey=
+RecaptchaSecretKey=
+Locale=en
+
