a1dacc4ff79630400b8b4f60e9bb841fe64401d9
Joel Feinstein <jnfeinstein@gmail.com>
2018-07-11 16:36:22 -0700

    Jade --> Pug
    Completes conversion of Jade to Pug.  The version of Jade (3.1.0)
    was years old and required dependencies with known security issues.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(i18n.init);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
