a8be411e6677d38f858c1c2003fef14dca986769
Outsider <outsideris@gmail.com>
2018-07-13 03:45:17 +0900

    delete unused script file
    because of 1f4d743
    
    Signed-off-by: Outsider <outsideris@gmail.com>

--- scripts/docker-deploy.sh
+++
@@ -1,8 +0,0 @@
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-
-docker build -t outsideris/slack-invite-automation:$TRAVIS_TAG .
-
-docker tag outsideris/slack-invite-automation:$TRAVIS_TAG outsideris/slack-invite-automation:latest
-
-docker push outsideris/slack-invite-automation:$TRAVIS_TAG
-docker push outsideris/slack-invite-automation:latest
