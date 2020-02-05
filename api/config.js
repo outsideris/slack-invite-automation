c2805eff19e38ec5826cf8c87c6c1bb75f9736b6
Joseph D. Purcell <josephdpurcell@gmail.com>
2020-02-05 08:15:11 -0500

    Set config.js values

--- api/config.js
+++ api/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  community: process.env.COMMUNITY_NAME || 'consciouscomm',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  slackUrl: process.env.SLACK_URL || 'consciouscomm.slack.com',
   // access token of slack
   // see https://github.com/outsideris/slack-invite-automation#issue-token
   //
