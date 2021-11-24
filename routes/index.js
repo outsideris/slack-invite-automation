caf2691e7b42067febae1a5be39d9e4a8bbf825b
Stefan Wallin <github@stefan-wallin.se>
2021-11-24 21:19:21 +0100

    Change how invite app works

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
+const fetch = require('cross-fetch');
 
 const config = require('../config');
 const { badge } = require('../lib/badge');
@@ -16,48 +16,67 @@ router.get('/', function(req, res) {
 
 router.post('/invite', function(req, res) {
   if (req.body.email && (!config.inviteToken || (!!config.inviteToken && req.body.token === config.inviteToken))) {
-    function doInvite() {
-      request.post({
-          url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
-          form: {
-            email: req.body.email,
-            token: config.slacktoken,
-            set_active: true
+    async function doInvite() {
+      const email = req.body.email;
+      const channelId = config.slackChannel;
+      const body = {
+        "channel": channelId,
+        "blocks": [
+          {
+              "type": "section",
+              "text": {
+                  "type": "mrkdwn",
+                  "text": `:eyes::wave:  *${email}* :wave::eyes:`
+              }
+          },
+          {
+              "type": "section",
+              "text": {
+                  "type": "mrkdwn",
+                  "text": `A person with email \`${email}\` has requested to join your workspace. Copy next message and post it to invite that person`
+              }
+          },
+          {
+              "type": "section",
+              "text": {
+                  "type": "mrkdwn",
+                  "text": `\`/invite ${email}\``
+              }
           }
-        }, function(err, httpResponse, body) {
-          // body looks like:
-          //   {"ok":true}
-          //       or
-          //   {"ok":false,"error":"already_invited"}
-          if (err) { return res.send('Error:' + err); }
-          body = JSON.parse(body);
-          if (body.ok) {
-            res.render('result', {
-              community: config.community,
-              message: 'Success! Check &ldquo;'+ req.body.email +'&rdquo; for an invite from Slack.'
-            });
-          } else {
-            let error = body.error;
-            if (error === 'already_invited' || error === 'already_in_team') {
-              res.render('result', {
-                community: config.community,
-                message: 'Success! You were already invited.<br>' +
-                        'Visit <a href="https://'+ config.slackUrl +'">'+ config.community +'</a>'
-              });
-              return;
-            } else if (error === 'invalid_email') {
-              error = 'The email you entered is an invalid email.';
-            } else if (error === 'invalid_auth') {
-              error = 'Something has gone wrong. Please contact a system administrator.';
-            }
-
-            res.render('result', {
-              community: config.community,
-              message: 'Failed! ' + error,
-              isFailed: true
-            });
+        ]
+      };
+      const url = 'https://'+ config.slackUrl + '/api/chat.postMessage';
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+            "Authorization": `Bearer ${config.slacktoken}`
           }
         });
+        if(response.ok) {
+          const data = await response.json();
+          if (data.ok) {
+            res.render('result', {
+              community: config.community,
+              message: "Your email &ldquo;" + req.body.email + "&rdquo; has been sent to our admin for invite. Keep a look out in your inbox for an invitation."
+            });
+          } else {
+            console.error(data);
+            throw new Error('Non-ok response')
+          }
+        }
+      } catch(err) {
+        console.error(err);
+        error = 'Something has gone wrong. Please try again at a later time.';
+        res.render('result', {
+          community: config.community,
+          message: 'Failed! ' + error,
+          isFailed: true
+        });
+
+      }
     }
     if (!!config.recaptchaSiteKey && !!config.recaptchaSecretKey) {
       request.post({
